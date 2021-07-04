a8e49fd04e4a8f8583441eb1f470135b2d37718d
Andrés Felipe <andrecamposer@gmail.com>
2021-07-04 14:21:10 -0500

    Removing module jswon web token

--- routes/orders.js
+++ routes/orders.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const { checkToken } = require("../authorization/checkToken");
 const router = express.Router();
 
 router.post("/", checkToken, (req, res) => {
+	/* console.log("res_id", req.params.id); */
 	console.log("Andres");
 	res.json({
 		mensaje: "Post fue creado",
